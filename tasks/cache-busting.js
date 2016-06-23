567d586ee3cb768532a0969ddd30b062e093bbbe
Collin Scangarella <collinscangarella@gmail.com>
2016-06-23 16:19:28 -0700

    Added minimatch support for file.
    File can now be a minimatch expression. This allows minifying directories and updating other source files with their updated paths. This is useful for angular templateUrls, it allows one to cache bust all html files and update the js files which declare them.
    
    I also added an option matchHashed. If match hashed is undefined or true we try to replace file-HASH.ext. Otherwise we only try to replace file.ext.

--- tasks/cache-busting.js
+++ tasks/cache-busting.js
@@ -6,43 +6,58 @@ module.exports = function (grunt) {
 		gruntTextReplace = require('grunt-text-replace/lib/grunt-text-replace');
 
 	grunt.registerMultiTask('cache-busting', 'Cache bust file and update references', function() {
-		var fileContents = grunt.file.read(this.data.file),
-			hash = crypto.createHash('md5').update(fileContents).digest("hex"),
-			outputDir = path.dirname(this.data.file),
-			fileExtension = path.extname(this.data.file),
-			replacementExtension = path.extname(this.data.replacement),
-			replacementWithoutExtension = this.data.replacement.slice(0, this.data.replacement.length - replacementExtension.length),
-			outputFile = outputDir + path.sep + replacementWithoutExtension + "-" + hash + fileExtension;
-
-		if (this.data.get_param){
-
-			gruntTextReplace.replace({
-				src: this.data.replace,
-				overwrite: true,
-				replacements: [{
-					from: new RegExp(this.data.replacement + "(\\?v=)?([a-z0-9]+)*"),
-					to: this.data.replacement + "?v=" + hash
-				}]
-			});
-
-		} else {
-			if (this.data.cleanup) {
-				var files = glob.sync(outputDir + path.sep + replacementWithoutExtension + "-*" + fileExtension);
-				files.forEach(function(file){
-					fs.unlink(file);
-				})
+		var filesToReplace = grunt.file.expand(this.data.file, 'isFile');
+		var hasReplacement = this.data.replacement !== undefined;
+		for (var i = 0; i < filesToReplace.length; i++) {
+			var replacement;
+			if (hasReplacement) {
+				replacement = this.data.replacement
+			} else {
+				replacement = path.basename(filesToReplace[i]);
 			}
-			fs.rename(this.data.file, outputFile);
-			var from = replacementWithoutExtension + (replacementExtension ? "((\-?)(.+)*)" + replacementExtension : '');
-			gruntTextReplace.replace({
-				src: this.data.replace,
-				overwrite: true,
-				replacements: [{
-					from: new RegExp(from),
-					to: replacementWithoutExtension + "-" + hash + replacementExtension
-				}]
-			});
+			var fileContents = grunt.file.read(filesToReplace[i]),
+				hash = crypto.createHash('md5').update(fileContents).digest("hex"),
+				outputDir = path.dirname(filesToReplace[i]),
+				fileExtension = path.extname(filesToReplace[i]),
+				replacementExtension = path.extname(replacement),
+				replacementWithoutExtension = replacement.slice(0, replacement.length - replacementExtension.length),
+				outputFile = outputDir + path.sep + replacementWithoutExtension + "-" + hash + fileExtension;
 
+			if (this.data.get_param){
+
+				gruntTextReplace.replace({
+					src: this.data.replace,
+					overwrite: true,
+					replacements: [{
+						from: new RegExp(replacement + "(\\?v=)?([a-z0-9]+)*"),
+						to: replacement + "?v=" + hash
+					}]
+				});
+
+			} else {
+				if (this.data.cleanup) {
+					var files = glob.sync(outputDir + path.sep + replacementWithoutExtension + "-*" + fileExtension);
+					files.forEach(function(file){
+						fs.unlink(file);
+					})
+				}
+				fs.rename(filesToReplace[i], outputFile);
+				var from;
+				if (this.data.matchHashed === undefined || this.data.matchHashed) {
+					from = replacementWithoutExtension + (replacementExtension ? "((\-?)(.+)*)" + replacementExtension : '');
+				} else {
+					from = replacementWithoutExtension + replacementExtension;
+				}
+				gruntTextReplace.replace({
+					src: this.data.replace,
+					overwrite: true,
+					replacements: [{
+						from: new RegExp(from),
+						to: replacementWithoutExtension + "-" + hash + replacementExtension
+					}]
+				});
+
+			}
 		}
 	});
 };
